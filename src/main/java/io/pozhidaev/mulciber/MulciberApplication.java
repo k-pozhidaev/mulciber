25b51d3ff7b70cfcfeacfbf40a4f8d1555c07ef8
Konstantin Pozhidaev <kos89jed@gmal.com>
2018-06-14 21:38:29 +0300

    Init.

---
+++ src/main/java/io/pozhidaev/mulciber/MulciberApplication.java
@@ -0,0 +1,12 @@
+package io.pozhidaev.mulciber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulciberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulciberApplication.class, args);
+	}
+}
---
+++ src/test/java/io/pozhidaev/mulciber/MulciberApplicationTests.java
@@ -0,0 +1,23 @@
+package io.pozhidaev.mulciber;
+
+import org.jooq.DSLContext;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MulciberApplicationTests {
+
+	@MockBean
+	protected DataSource dataSource;
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
