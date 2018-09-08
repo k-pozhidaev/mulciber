c6626d57f30a4bc8af424e052a9c886bc2f61eeb
Konstantin Pozhidaev <kos89jed@gmail.com>
2018-09-08 18:48:17 +0300

    Themeleaf added.

--- src/main/java/io/pozhidaev/mulciber/config/SecurityConfig.java
+++ src/main/java/io/pozhidaev/mulciber/config/SecurityConfig.java
@@ -4,14 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 
 @Configuration
 @EnableResourceServer
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    public void configure(HttpSecurity http) throws Exception {
         http
                 .antMatcher("/**")
                 .authorizeRequests().antMatchers("/login").anonymous()
