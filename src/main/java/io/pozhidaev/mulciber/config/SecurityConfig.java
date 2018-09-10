cc598dfb96e7ae31628eb93b5d6403b809a03246
Konstantin Pozhidaev <kos89jed@gmail.com>
2018-09-10 17:01:57 +0300

    Sec conf.

--- src/main/java/io/pozhidaev/mulciber/config/SecurityConfig.java
+++ src/main/java/io/pozhidaev/mulciber/config/SecurityConfig.java
@@ -1,8 +1,12 @@
 package io.pozhidaev.mulciber.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
@@ -17,6 +21,21 @@ public class SecurityConfig extends ResourceServerConfigurerAdapter {
                 .antMatcher("/**")
                 .authorizeRequests().antMatchers("/login").anonymous()
                 .antMatchers("/logout").authenticated();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth)
+            throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("user").password(passwordEncoder().encode("password")).roles("USER")
+                .and()
+                .withUser("admin").password(passwordEncoder().encode("admin")).roles("ADMIN");
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 
