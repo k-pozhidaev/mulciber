17e435703be03aa178bc17a9bae5eb5eebc5f5b8
Konstantin Pozhidaev <k.pozhidaev@sap.com>
2018-09-27 19:18:16 +0300

    OAuth service config.

--- src/main/java/io/pozhidaev/mulciber/controller/MainController.java
+++ src/main/java/io/pozhidaev/mulciber/controller/MainController.java
@@ -1,9 +1,15 @@
 package io.pozhidaev.mulciber.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/")
 public class MainController {
+
+    @GetMapping
+    public String main(){
+        return "index";
+    }
 }
