f786873d32618e98e43565a7faf2181affe8c48e
Konstantin Pozhidaev <kos89jed@gmail.com>
2018-09-28 17:42:51 +0300

    removed extra

--- src/main/java/io/pozhidaev/mulciber/config/SecurityConfig.java
+++ src/main/java/io/pozhidaev/mulciber/config/SecurityConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
