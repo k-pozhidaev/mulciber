888424ebf68e13dfcd208b9d8f34a49d31f702df
Konstantin Pozhidaev <kos89jed@gmail.com>
2019-12-07 20:42:22 +0200

    serv

--- main.go
+++ main.go
@@ -1,9 +1,20 @@
 package main
 
-import "os"
+import (
+	"net/http"
+	"os"
+)
 
 func main() {
 	for _, arg := range os.Args {
 		println(arg)
 	}
+	http.HandleFunc("/protected", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, I'm protected"))
+	})
+
+
+	http.HandleFunc("/safe", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("test"))
+	})
 }
